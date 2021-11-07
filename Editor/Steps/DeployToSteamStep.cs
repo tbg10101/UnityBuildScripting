37316024656b6011999f3a4b87bdf1f0e0394493
Tristan Bellman-Greenwood <tbg10101@gmail.com>
2021-11-06 23:10:38 -0400

    synchronizing Steam deployment

--- Editor/Steps/DeployToSteamStep.cs
+++ Editor/Steps/DeployToSteamStep.cs
@@ -7,6 +7,9 @@ using Debug = UnityEngine.Debug;
 
 namespace Software10101.BuildScripting.Editor {
     public class DeployToSteamStep : AbstractBuildStep {
+        // Ensure that we only run one of these at a time or Steam can fail.
+        private static readonly object LockObject = new object();
+
         [NotNull]
         public static string SteamCliPath = @"C:\Program Files\Steamworks SDK\tools\ContentBuilder\builder\steamcmd.exe";
 
@@ -47,136 +50,137 @@ namespace Software10101.BuildScripting.Editor {
         }
 
         public override void Execute(string outputDir, AbstractBuildPipeline pipeline) {
-            // create the VDF file for Steam
-            StringBuilder sb = new StringBuilder();
+            lock (LockObject) {
+                // create the VDF file for Steam
+                StringBuilder sb = new StringBuilder();
 
-            sb.AppendLine("\"AppBuild\"");
-            sb.AppendLine("{");
-            {
-                sb.AppendLine($"	\"AppID\" \"{_appId}\"");
-                sb.AppendLine($"	\"Desc\" \"UnityBuildScripting-{pipeline.Name}\"");
-
-                sb.AppendLine($"	\"ContentRoot\" \"{_contentRoot}\"");
-                sb.AppendLine("	\"BuildOutput\" \"steam_logs\"");
-
-                sb.AppendLine("	\"Depots\"");
-                sb.AppendLine("	{");
+                sb.AppendLine("\"AppBuild\"");
+                sb.AppendLine("{");
                 {
-                    sb.AppendLine($"		\"{_depotId}\"");
-                    sb.AppendLine("		{");
+                    sb.AppendLine($"	\"AppID\" \"{_appId}\"");
+                    sb.AppendLine($"	\"Desc\" \"UnityBuildScripting-{pipeline.Name}\"");
+
+                    sb.AppendLine($"	\"ContentRoot\" \"{_contentRoot}\"");
+                    sb.AppendLine("	\"BuildOutput\" \"steam_logs\"");
+
+                    sb.AppendLine("	\"Depots\"");
+                    sb.AppendLine("	{");
                     {
-                        sb.AppendLine("			\"FileMapping\"");
-                        sb.AppendLine("			{");
+                        sb.AppendLine($"		\"{_depotId}\"");
+                        sb.AppendLine("		{");
                         {
-                            sb.AppendLine("				\"LocalPath\" \"*\"");
-                            sb.AppendLine("				\"DepotPath\" \".\"");
-                            sb.AppendLine("				\"recursive\" \"1\"");
+                            sb.AppendLine("			\"FileMapping\"");
+                            sb.AppendLine("			{");
+                            {
+                                sb.AppendLine("				\"LocalPath\" \"*\"");
+                                sb.AppendLine("				\"DepotPath\" \".\"");
+                                sb.AppendLine("				\"recursive\" \"1\"");
+                            }
+                            sb.AppendLine("			}");
+                            sb.AppendLine("			\"FileExclusion\" \"*.pdb\"");
                         }
-                        sb.AppendLine("			}");
-                        sb.AppendLine("			\"FileExclusion\" \"*.pdb\"");
+                        sb.AppendLine("		}");
                     }
-                    sb.AppendLine("		}");
+                    sb.AppendLine("	}");
                 }
-                sb.AppendLine("	}");
-            }
-            sb.AppendLine("}");
+                sb.AppendLine("}");
 
-            const string vdfFileName = "app_build.vdf";
-            string vdfPath = Path.Combine(outputDir, vdfFileName);
-            string vdfFullPath = Path.GetFullPath(vdfPath);
+                const string vdfFileName = "app_build.vdf";
+                string vdfPath = Path.Combine(outputDir, vdfFileName);
+                string vdfFullPath = Path.GetFullPath(vdfPath);
 
-            File.WriteAllText(vdfPath, sb.ToString());
+                File.WriteAllText(vdfPath, sb.ToString());
 
-            // write SteamGuard files
-            string ssfnDestination = Path.GetFullPath(Path.Combine(SteamCliPath, "..", Path.GetFileName(SsfnPath)));
-            string ssfnBackup = $"{ssfnDestination}.bak";
+                // write SteamGuard files
+                string ssfnDestination = Path.GetFullPath(Path.Combine(SteamCliPath, "..", Path.GetFileName(SsfnPath)));
+                string ssfnBackup = $"{ssfnDestination}.bak";
 
-            if (File.Exists(ssfnDestination)) {
-                if (File.Exists(ssfnBackup)) {
-                    File.Delete(ssfnBackup);
-                }
+                if (File.Exists(ssfnDestination)) {
+                    if (File.Exists(ssfnBackup)) {
+                        File.Delete(ssfnBackup);
+                    }
 
-                File.Move(ssfnDestination, ssfnBackup);
-            }
-
-            File.Copy(SsfnPath, ssfnDestination);
-
-            string configParent = Path.GetFullPath(Path.Combine(SteamCliPath, "..", "config"));
-
-            if (!Directory.Exists(configParent)) {
-                Directory.CreateDirectory(configParent);
-            }
-
-            string configDestination = Path.Combine(configParent, Path.GetFileName(ConfigVdfPath));
-            string configBackup = $"{configDestination}.bak";
-
-            if (File.Exists(configDestination)) {
-                if (File.Exists(configBackup)) {
-                    File.Delete(configBackup);
+                    File.Move(ssfnDestination, ssfnBackup);
                 }
 
-                File.Move(configDestination, configBackup);
-            }
+                File.Copy(SsfnPath, ssfnDestination);
 
-            File.Copy(ConfigVdfPath, configDestination);
+                string configParent = Path.GetFullPath(Path.Combine(SteamCliPath, "..", "config"));
 
-            // execute deployment
-            string args = $"+@ShutdownOnFailedCommand 1 +@NoPromptForPassword 1 +login \"{_accountName}\" \"{_accountPassword}\" +run_app_build \"{vdfFullPath}\" +quit";
-            Debug.Log($"Beginning deployment: {SteamCliPath} {args}");
+                if (!Directory.Exists(configParent)) {
+                    Directory.CreateDirectory(configParent);
+                }
 
-            using (Process deployProcess = new Process {
-                       StartInfo = new ProcessStartInfo {
-                           FileName = SteamCliPath,
-                           Arguments = args,
-                           CreateNoWindow = true,
-                           UseShellExecute = false,
-                           RedirectStandardOutput = true,
-                           RedirectStandardError = true
+                string configDestination = Path.Combine(configParent, Path.GetFileName(ConfigVdfPath));
+                string configBackup = $"{configDestination}.bak";
+
+                if (File.Exists(configDestination)) {
+                    if (File.Exists(configBackup)) {
+                        File.Delete(configBackup);
+                    }
+
+                    File.Move(configDestination, configBackup);
+                }
+
+                File.Copy(ConfigVdfPath, configDestination);
+
+                // execute deployment
+                string args =
+                    $"+@ShutdownOnFailedCommand 1 +@NoPromptForPassword 1 +login \"{_accountName}\" \"{_accountPassword}\" +run_app_build \"{vdfFullPath}\" +quit";
+                Debug.Log($"Beginning deployment: {SteamCliPath} {args}");
+
+                using (Process deployProcess = new Process {
+                           StartInfo = new ProcessStartInfo {
+                               FileName = SteamCliPath,
+                               Arguments = args,
+                               CreateNoWindow = true,
+                               UseShellExecute = false,
+                               RedirectStandardOutput = true,
+                               RedirectStandardError = true
+                           }
                        }
+                      ) {
+                    deployProcess.OutputDataReceived += (_,  message) => {
+                        if (string.IsNullOrEmpty(message.Data)) {
+                            return;
+                        }
+
+                        Debug.Log(message.Data);
+                    };
+
+                    deployProcess.ErrorDataReceived += (_, message) => {
+                        if (string.IsNullOrEmpty(message.Data)) {
+                            return;
+                        }
+
+                        Debug.LogError(message.Data);
+                    };
+
+                    deployProcess.Start();
+                    deployProcess.BeginOutputReadLine();
+                    deployProcess.BeginErrorReadLine();
+
+                    deployProcess.WaitForExit();
+
+                    if (deployProcess.ExitCode == 0) {
+                        Debug.Log("Deployment exited successfully.");
+                    } else {
+                        throw new Exception($"Deployment exited with code: {deployProcess.ExitCode}");
                     }
-            ) {
-                deployProcess.OutputDataReceived += (_,  message) => {
-                    if (string.IsNullOrEmpty(message.Data))
-                    {
-                        return;
-                    }
-
-                    Debug.Log(message.Data);
-                };
-
-                deployProcess.ErrorDataReceived += (_, message) => {
-                    if (string.IsNullOrEmpty(message.Data))
-                    {
-                        return;
-                    }
-
-                    Debug.LogError(message.Data);
-                };
-
-                deployProcess.Start();
-                deployProcess.BeginOutputReadLine();
-                deployProcess.BeginErrorReadLine();
-
-                deployProcess.WaitForExit();
-
-                if (deployProcess.ExitCode == 0) {
-                    Debug.Log("Deployment exited successfully.");
-                } else {
-                    throw new Exception($"Deployment exited with code: {deployProcess.ExitCode}");
                 }
-            }
 
-            // clean up
-            File.Delete(ssfnDestination);
+                // clean up
+                File.Delete(ssfnDestination);
 
-            if (File.Exists(ssfnBackup)) {
-                File.Move(ssfnBackup, ssfnDestination);
-            }
+                if (File.Exists(ssfnBackup)) {
+                    File.Move(ssfnBackup, ssfnDestination);
+                }
 
-            File.Delete(configDestination);
+                File.Delete(configDestination);
 
-            if (File.Exists(configBackup)) {
-                File.Move(configBackup, configDestination);
+                if (File.Exists(configBackup)) {
+                    File.Move(configBackup, configDestination);
+                }
             }
         }
     }
